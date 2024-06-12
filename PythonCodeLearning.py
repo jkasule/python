8f331ffec51660c93d72d46d0e2c45a1b464557c
jkasule <kasulejeri@gmail.com>
2024-06-12 14:35:56 +0300

    Add files via upload

---
+++ PythonCodeLearning.py
@@ -0,0 +1,85 @@
+#print("hello there")
+#print("school is back")
+#print("The work has started: sit tight")
+
+#name = "jerry is a fine dude"
+#print(name)
+#print("did you know "+name)
+#print(type(name))
+#STRINGS
+#fname= "jerry"
+#lname= "kasule"
+#full_name= fname+ " " + lname
+##print(full_name)
+
+#INTEGERS
+#age = 21
+#age += 1
+#print(age)
+# to print interger with string, you convert integer to string
+#print("my age is absolutly " + str(age))
+
+#BOOLEANS
+#human = True
+#print("Are you Human: " + str(human))
+
+#MUTLIPLE ASSIGNMENT
+#name, age, attractive = "bob", 21, True
+
+#print(name+"'s age is: "+str(age)+ " he is attractive, yah? "+str(attractive))
+
+#bob = john = patrick = 42
+#print(john)
+#print("John's age is " + str(john))
+
+#z = 3
+#x = 4
+#y = 5
+#print(z*x*y)
+
+#print(z,x,y)
+
+#name = input("your name?")
+#print("hullo "+ name)
+import math
+
+#pi = 3.14
+#print(math.sqrt(pi))
+
+#website = "http://google.com"
+#slice = slice(7,-4)
+#print(website[slice])
+
+#age= int(input("your age?"))
+#if age >= 18:
+ #   print("laba ka aludat")
+#elif age< 18:
+ #   print("ono yanga")
+#def is_odd(number):
+#    return number % 2 == 1
+#number = int(input("enter a number"))
+
+#if is_odd(number):
+#     print("odd nmber")
+
+#name = ""
+#while len(name) == 0:
+#    name = input("enter name: ")
+#print("hello " + name)
+#import time
+
+#for second in range(10,0,-1):
+#    print(second)
+#   time.sleep(1)
+#print("count done")
+
+#food = ["rice", " matooke", " ice cream"]
+#for x in food:
+#   print(x,end="")
+
+capitals = {'Uganda':'kampala',
+            'Kenya':'mombasa',
+            'usa': 'dc',
+            'russia': 'moscow'}
+
+print(capitals['Kenya'])
