3be2374a3cc50d1d9d9396b3b6e2937fd238b390
jkasule <kasulejeri@gmail.com>
2024-06-17 17:09:52 +0300

    Update simple-calculator.py
    Creates a simple calculator

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,6 +1,6 @@
-operator = input("enter operator, ie + - * / ")
-num1 = float(input("enter 1st number"))
-num2 = float(input("enter 2nd number"))
+operator = input("enter operator, ie + - * /:  ")
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
 
 if operator == "+":
     result = num1 + num2
@@ -11,8 +11,13 @@ elif operator == "-":
 elif operator == "*":
     result = num1 * num2
     print(round(result, 3))
+
 elif operator == "/":
-    result = num1 / num2
-    print(round(result, 3))
+    if num2 == 0:
+        print("IMPOSSIBLE")
+    else:
+        result = num1 / num2
+        print(round(result, 3))
+
 else:
-    print(f"{operator} is not a valid operator")
+    print("operator " + str(operator) + " is not a valid operator")
